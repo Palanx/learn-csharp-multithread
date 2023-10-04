056bd1fa2637767d4abfb8b96524cf436c6ec30a
palanx <tmunizq@gmail.com>
2023-10-04 19:38:46 -0300

    processes-and-threads completed

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # learn-csharp-multithread
 Repo just to learn Multi-Thread in C#
+
+## Project <b>processes-and-threads</b>: Thread API, Thread Pool, APM & EAP
+There is a [Link](https://www.udemy.com/course/parallel-csharp/learn/lecture/11126220) to the course.
+- How to start a process, how to chanfe its priority, kill a process, query the OS for existing process and such.
+- API of the old-school Thread class.
+- The relation of thread and a very old COM technology.
+- What is a thread pool and how to use thread pool threads?
+- Two types of thread pool threads: workers and I/O.
+- I/O-bound operations in detail.
+- APM (Asynchronous programming model) and EAP (event-based asynchronous patterns). 
---
+++ multi-thread-course/processes-and-threads/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace oricesses_and_threads
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            PrintNumbersUsingThread();
+
+            Console.Read();
+        }
+
+        #region Old Thread Example
+        private static void PrintNumbersUsingThread()
+        {
+            Console.WriteLine("Even:");
+            Thread t1 = new Thread(PrintNumbers);
+            t1.IsBackground = true; // If the app stops all the background threads will stop
+            t1.Start(true);
+            t1.Priority = ThreadPriority.Highest;
+
+            if (t1.Join(TimeSpan.FromSeconds(5))) // Block the calling thread (main in this case) until time-out or thread terminates
+            {
+                Console.WriteLine("Thread terminates at time!");
+            }
+            else
+            {
+                Console.WriteLine("Thread time-out!");
+            }
+
+            Console.WriteLine("Odd:");
+            PrintNumbers(false);
+        }
+
+        private static void PrintNumbers(object arg)
+        {
+            Console.WriteLine($"Current Thread ID: {Thread.CurrentThread.ManagedThreadId}");
+            bool isEven = (bool) arg;
+            for (int i = 0; i < 100; i++)
+            {
+                if ((i%2 == 0) != isEven) continue;
+
+                Console.WriteLine(i);
+            }
+        }
+        #endregion Old Thread Example
+
+        #region Process Example
+        private static void OpenAndCloseNotepad()
+        {
+            Process notepadApp = OpenNotepad();
+            KillProcessByName(notepadApp.ProcessName);
+        }
+
+        private static Process OpenNotepad()
+        {
+            Process.Start("notepad.exe");
+
+            Process app = new Process()
+            {
+                StartInfo =
+                {
+                    FileName = @"notepad.exe",
+                    Arguments = @"C:/tmp/hello-world.txt"
+                }
+            };
+            app.Start();
+
+            app.PriorityClass = ProcessPriorityClass.RealTime;
+            return app;
+        }
+
+        private static void WaitProcessForExit(Process process)
+        {
+            process.WaitForExit();
+            Console.WriteLine("Process closed!");
+        }
+
+        private static void KillProcessByName(string name)
+        {
+            Process[] processes = Process.GetProcesses();
+            foreach (Process process in processes)
+            {
+                if (process.ProcessName != name) continue;
+
+                process.Kill();
+            }
+        }
+        #endregion Process Example
+    }
+}
